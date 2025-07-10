5bb67df74d263b78dc809c1d28eee996f7dd35f7
Bryan Kariuki <69392586+bryanbudah@users.noreply.github.com>
2025-07-09 21:47:52 -0700

    Add files via upload

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,29 @@
+import seed
+
+def stream_user_ages():
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+    cursor.execute("SELECT age FROM user_data")
+
+    for row in cursor:
+        yield row[0]
+
+    cursor.close()
+    connection.close()
+
+def calculate_average_age():
+    total_age = 0
+    count = 0
+
+    for age in stream_user_ages():
+        total_age += age
+        count += 1
+
+    if count > 0:
+        average = total_age / count
+        print(f"Average age of users: {average:.2f}")
+    else:
+        print("No users found.")
+
+if __name__ == "__main__":
+    calculate_average_age()
